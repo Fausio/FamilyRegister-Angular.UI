c7b34bb8896b4873f5cbd11a9234710d06adf7c2
fausio luis <fausioluis@live.com>
2023-07-29 15:08:45 +0200

    container

--- src/app/components/family/family-pagination/family-pagination.component.ts
+++ src/app/components/family/family-pagination/family-pagination.component.ts
@@ -1,11 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Family } from 'src/app/models/Family';
 
 @Component({
   selector: 'app-family-pagination',
   templateUrl: './family-pagination.component.html',
   styleUrls: ['./family-pagination.component.css']
 })
-export class FamilyPaginationComponent {
+export class FamilyPaginationComponent implements OnInit {
 
-  
+
+  families: Family[] = [
+   
+    {
+      id: 1,
+      name: "",
+      email: '',
+      money: 123,
+      phone: ''
+    },
+    {
+      id: 2,
+      name: "",
+      email: '',
+      money: 456,
+    },
+    {
+      id: 3,
+      name: "",
+      email: '',
+      money: 678,
+    }
+
+  ];
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+
+
+
+  private seedFamily() {
+
+    for (let index = 0; index < 100; index++) {
+
+     /*  this.families.push(new Family(){
+        id: index + 1,
+        name: 'xsxs' + index,
+        money: 678,
+      }) */
+
+    }
+
+  }
+
 }
--- src/app/models/Family.ts
+++ src/app/models/Family.ts
@@ -1,9 +1,11 @@
 export interface Family {
 
-    name : string;
-    email: string;
-    phone: string;
-    address: string;
+    id: number,
+    name? : string;
+    email?: string;
+    phone?: string;
+    address?: string;
     money: number;
+    members?: number;
 
 }
