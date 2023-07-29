b80af9b4565cd82c5079b0d25561d07d24a3e748
fausio luis <fausioluis@live.com>
2023-07-29 14:16:27 +0200

    Family

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FamilyPaginationComponent } from './components/family/family-pagination/family-pagination.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: FamilyPaginationComponent
+  },
+  {
+    path: 'family-pagination',
+    component: FamilyPaginationComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/models/Family.ts
@@ -0,0 +1,9 @@
+export interface Family {
+
+    name : string;
+    email: string;
+    phone: string;
+    address: string;
+    money: number;
+
+}
